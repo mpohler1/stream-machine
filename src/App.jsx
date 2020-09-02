940082bdaa3e4895c9e91592411d68056e70c823
mpohler1 <masonpohler@gmail.com>
2020-09-02 17:35:49 -0500

    fixed Screens within Stack Navigator

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,13 @@ import PlaylistBuilder from "./components/PlaylistBuilder";
 import Playlist from "./components/Playlist";
 import Player from "./components/Player";
 import Splash from "./components/Splash";
-import {SPLASH, LOGIN, PLAYLIST_SELECTOR, PLAYLIST_BUILDER, PLAYLIST, PLAYER} from "./resources/routes";
+import {
+    SPLASH_ROUTE,
+    LOGIN_ROUTE,
+    PLAYLIST_SELECTOR_ROUTE,
+    PLAYLIST_BUILDER_ROUTE,
+    PLAYLIST_ROUTE,
+    PLAYER_ROUTE} from "./resources/routes";
 
 const Stack = createStackNavigator();
 
@@ -19,13 +25,13 @@ class App extends React.Component {
     return (
         <Provider store={store}>
             <NavigationContainer>
-                <Stack.Navigator initialRouteName={SPLASH}>
-                    <Stack.screen name={SPLASH} component={Splash} />
-                    <Stack.screen name={LOGIN} component={Login} />
-                    <Stack.screen name={PLAYLIST_SELECTOR} component={PlaylistSelector} />
-                    <Stack.screen name={PLAYLIST_BUILDER} component={PlaylistBuilder} />
-                    <Stack.screen name={PLAYLIST} component={Playlist} />
-                    <Stack.screen name={PLAYER} component={Player}/>
+                <Stack.Navigator initialRouteName={SPLASH_ROUTE}>
+                    <Stack.Screen name={SPLASH_ROUTE} component={Splash} />
+                    <Stack.Screen name={LOGIN_ROUTE} component={Login} />
+                    <Stack.Screen name={PLAYLIST_SELECTOR_ROUTE} component={PlaylistSelector} />
+                    <Stack.Screen name={PLAYLIST_BUILDER_ROUTE} component={PlaylistBuilder} />
+                    <Stack.Screen name={PLAYLIST_ROUTE} component={Playlist} />
+                    <Stack.Screen name={PLAYER_ROUTE} component={Player}/>
                 </Stack.Navigator>
             </NavigationContainer>
         </Provider>
--- src/resources/routes.js
+++ src/resources/routes.js
@@ -1,6 +1,6 @@
-export const SPLASH = "Splash";
-export const LOGIN = "Login";
-export const PLAYLIST_SELECTOR = "PlaylistSelector";
-export const PLAYLIST_BUILDER = "PlaylistBuilder";
-export const PLAYLIST = "Playlist";
-export const PLAYER = "Player";
+export const SPLASH_ROUTE = "Splash";
+export const LOGIN_ROUTE = "Login";
+export const PLAYLIST_SELECTOR_ROUTE = "PlaylistSelector";
+export const PLAYLIST_BUILDER_ROUTE = "PlaylistBuilder";
+export const PLAYLIST_ROUTE = "Playlist";
+export const PLAYER_ROUTE = "Player";
